7273c8dbd31761479f988a37be46b939d9805518
Souza1981 <souza1981@gmail.com>
2020-02-25 12:14:44 -0300

    ajustes nos testes unitários

--- BIL.API/Startup.cs
+++ BIL.API/Startup.cs
@@ -78,7 +78,7 @@ namespace BIL_API
 
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline. 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
