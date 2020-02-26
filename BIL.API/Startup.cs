516b72d9738ef1e27835225f9d9fad14d3102acc
Souza1981 <souza1981@gmail.com>
2020-02-25 22:38:34 -0300

    inicio da implementação do repositório de editoras

--- BIL.API/Startup.cs
+++ BIL.API/Startup.cs
@@ -24,7 +24,7 @@ namespace BIL_API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container. 
         public void ConfigureServices(IServiceCollection services)
         {
             //var connection = @"Server=db;Database=bildb;User=sa;Password=P@assWord1054;";
